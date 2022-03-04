867615360d95c80144e46720e8bcad4e18727ebd
Deepak Kumar Sinha <sinhadeepak1115study@gmail.com>
2022-03-05 00:17:19 +0530

    add axios and request.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import API_KEY from './requests'
 
 function App() {
+  // console.log(process.env.REACT_APP_API_KEY)
+  console.log(API_KEY);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hey lets bulid Netflix Front-End today.</h1>
     </div>
   );
 }
