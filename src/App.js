d8d3caced7b5fe2d78c1544e753003dbf7047f75
Deepak Kumar Sinha <sinhadeepak1115study@gmail.com>
2022-03-05 11:03:23 +0530

    added css to app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import './App.css';
-import Row  from './Row';
-import requests from './requests';
+import React from "react";
+import "./App.css";
+import Row from "./Row";
+import requests from "./requests";
 
 function App() {
   // console.log(API_KEY);
   return (
     <div className="App">
       <h1>Hey lets bulid Netflix Front-End today.🚀</h1>
-      <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals}/>
-      <Row title="Trending " fetchUrl={requests.fetchTrending}/>
+      <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="Trending " fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentary" fetchUrl={requests.fetchDocumantaries} />
     </div>
   );
 }
--- src/Row.js
+++ src/Row.js
@@ -1,22 +1,35 @@
-import React, {useState, useEffect} from 'react'
-import axios from './axios';
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import "./Row.css";
 
-function Row({title, fetchUrl}) {
-    const [movie, setMovies] = useState([]);
-    useEffect(()=>{
-        async function fetchData(){
-            const request = await axios.get(fetchUrl);
-            console.log(request)
-            return request; 
-        }
-        fetchData();
-    }, []);
+const base_url = "https://image.tmdb.org/t/p/original/";
 
-    return (
-    <div>
-        <h2>{title}</h2>
+function Row({ title, fetchUrl }) {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+  console.table(movies);
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            className="row__poster"
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
     </div>
-    )
+  );
 }
 
 export default Row;
