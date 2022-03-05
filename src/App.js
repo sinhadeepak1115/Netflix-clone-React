852c3c59c6e1f58864bd127fce1e5a019073c3ee
Deepak Kumar Sinha <sinhadeepak1115study@gmail.com>
2022-03-05 18:55:29 +0530

    adding banner to clone.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import "./App.css";
 import Row from "./Row";
 import requests from "./requests";
+import Banner from "./Banner";
 
 function App() {
   // console.log(API_KEY);
   return (
     <div className="App">
-      <h1>Hey lets bulid Netflix Front-End today.🚀</h1>
+      <Banner />
       <Row
         title="Netflix Originals"
         fetchUrl={requests.fetchNetflixOriginals}
---
+++ src/Banner.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import requests from "./requests";
+
+function Banner() {
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchComedyMovies);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+      return request;
+    }
+    fetchData();
+  }, []);
+
+  console.log(movie.backdrop_path);
+
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`, backgroundPosition: "center center",
+      }}
+    >
+      <div className="banner__contents">
+        <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
+      </div>
+    </header>
+  );
+}
+
+export default Banner;
